5bcc1b0287778d35f289f66ac4dba487c86ed359
Ghita Hatimi <121192625+gh-ita@users.noreply.github.com>
2023-09-17 17:20:49 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = "internship.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = {
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
