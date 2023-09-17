c9b0478ebc8d69fdce873f292f42b8e8451e5fdc
Ghita Hatimi <121192625+gh-ita@users.noreply.github.com>
2023-09-17 16:32:31 +0100

    Update build_files.sh

--- internship/build_files.sh
+++ internship/build_files.sh
@@ -1,3 +1,6 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
-python3.9 manage.py runserver
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
